69a34175c9a4bbe995df436f8ca699bed7788a41
ETdoFresh <ETdoFresh@gmail.com>
2021-04-17 18:56:53 -0500

    Level 1 Scene has not been completely prefab-ized for multiple developers to work in. Restored some graphic fidelity.

--- Assets/InGame/Characters/Core/Scripts/MouseLook.cs
+++ Assets/InGame/Characters/Core/Scripts/MouseLook.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
-    public Transform playerBody;
-    private float xRotation = 0f;
     public bool inInterface = false;
-    
+    private Character character;
+    private Camera _camera;
+    private float xRotation = 0f;
+
     // Start is called before the first frame update
     void Awake()
     {
+        character = Character.GetCharacter(this);
+        _camera = Character.GetComponent<Camera>(this);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         HexagonPuzzleManager.onHasEnteredInterface += InInterface;
@@ -23,18 +26,17 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!inInterface)
-        {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (!character) return;
+        if (!_camera) return;
+        if (inInterface) return;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-            playerBody.Rotate(Vector3.up * mouseX);
-        }
+        _camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        character.transform.Rotate(Vector3.up * mouseX);
     }
 
     void InInterface(bool hasEntered)
