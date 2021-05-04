51282354141fc4f82077252fac711c377b476a74
ETdoFresh <ETdoFresh@gmail.com>
2021-05-04 17:50:54 -0500

    Add MainCamera.asset and RegisterMainCamera.cs. However, Camera is still a child of Character for now.

---
+++ Assets/InGame/Cameras/MainCamera.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace InGame.Cameras
+{
+    public class MainCamera : ScriptableObject
+    {
+        public Camera value;
+    }
+}
---
+++ Assets/InGame/Cameras/RegisterMainCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace InGame.Cameras
+{
+    public class RegisterMainCamera : MonoBehaviour
+    {
+        [SerializeField] private MainCamera mainCamera;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void OnEnable()
+        {
+            mainCamera.value = _camera;
+        }
+
+        private void OnDisable()
+        {
+            if (mainCamera.value == _camera) 
+                mainCamera.value = null;
+        }
+    }
+}
