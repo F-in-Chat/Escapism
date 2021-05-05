c9ccd5829dcda6c746109ed7199d869e354d2647
ETdoFresh <ETdoFresh@gmail.com>
2021-05-04 20:06:25 -0500

    Add Health Collectible/Collector. Add mesh colliders to walls. Add Sound Effect Scriptable Object for one-shots.

---
+++ Assets/InGame/Collectibles/Scripts/Collectible.cs
@@ -0,0 +1,36 @@
+﻿using InGame.Characters.Core.Scripts;
+using Parents;
+using SoundEffects;
+using UnityEngine;
+
+namespace InGame.Collectibles.Scripts
+{
+    public class Collectible : MonoBehaviour
+    {
+        [SerializeField] private SoundEffect collectSound;
+        [SerializeField] private GameObject collectParticles;
+        private Transform item;
+
+        private void Awake()
+        {
+            item = Parent.GetComponent<Transform>(this);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var collector = other.GetComponent<Collector>();
+            if (!collector) return;
+            collector.Collect(this);
+            if (collectSound) collectSound.Play(transform.position);
+            SpawnParticles();
+            Destroy(item.gameObject);
+        }
+
+        private void SpawnParticles()
+        {
+            if (!collectParticles) return;
+            var particles = Instantiate(collectParticles);
+            particles.transform.position = transform.position;
+        }
+    }
+}
