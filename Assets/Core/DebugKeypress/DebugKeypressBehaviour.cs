16235d6db097eb5109294c8eb6946941828c0b72
ETdoFresh <ETdoFresh@gmail.com>
2021-05-07 20:24:27 -0500

    Add 3 Temporary Sound Effects. Add SceneLoader.asset. Add DebugKeypressBehaviour.cs. Add Editor for SoundEffect.cs.

---
+++ Assets/Core/DebugKeypress/DebugKeypressBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DebugKeypress
+{
+    public class DebugKeypressBehaviour : MonoBehaviour
+    {
+        [SerializeField] private KeyCode keyCode;
+        [SerializeField] private UnityEvent onKeyPress;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(keyCode))
+                onKeyPress.Invoke();
+        }
+    }
+}
